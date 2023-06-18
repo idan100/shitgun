3ae6c4a90a9e07cea6561dca4380ddf86533887f
idan efraim <idan1000011@walla.com>
2023-06-18 21:29:05 +0300

    success in pulling from db

--- db.js
+++ db.js
@@ -1,19 +1,50 @@
 const dotenv = require("dotenv");
-const { Sequelize } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 
 dotenv.config();
 
 const sequelize = new Sequelize(`postgres://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGSERVER}:${process.env.PGPORT}/${process.env.PGDBNAME}`)
 
-const tesDB = async () => {
+const Person = sequelize.define('Person', {
+  id: {
+    type: DataTypes.STRING,
+    primaryKey: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  phone_number: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+},
+{
+    tableName:"person",
+    schema:"keys"
+}
+);
+
+
+const testDB = async () => {
   try {
     await sequelize.authenticate();
     console.log('Connection has been established successfully.');
-    //sequelize.close()
+    
+    const persons = await Person.findAll({
+      attributes: { exclude: ['createdAt', 'updatedAt'] }
+    });
+    //console.log(persons.every(person => person instanceof Person)); // true
+    console.log("All persons:", JSON.stringify(persons, null, 2));
+    
+    sequelize.close()
   } catch (error) {
     console.error('Unable to connect to the database:', error);
   }
 }
 
-tesDB();
-
+testDB();
