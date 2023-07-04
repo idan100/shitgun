89570092c89df2345ce3e4aaa99afcedb6e88fdf
idan efraim <idan1000011@walla.com>
2023-07-04 20:34:30 +0300

    updated pull on date change

--- front_new/src/components/calander/calander.js
+++ front_new/src/components/calander/calander.js
@@ -17,6 +17,7 @@ export default function BasicDateCalendar() {
       <LocalizationProvider dateAdapter={AdapterDayjs}>
          <DateCalendar value={value} onChange={(newValue) => {
             setLocalStorage(newValue);
+            window.location.reload();
             
             //axios.get(`${process.env.REACT_APP_API}/schedules/free/${localStorage.getItem('class')}/${localStorage.getItem('date')}`)
             //   .then(res => {
