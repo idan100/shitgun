dc53d9e236a96de964450b6598b1155aa53aede6
Yuval Tanami <yuval26602@gmail.com>
2023-07-04 20:37:51 +0300

    ddl

--- back/DDL.sql
+++ back/DDL.sql
@@ -1,23 +1,22 @@
-DROP TABLE IF EXISTS keys.person;
-CREATE TABLE IF NOT EXISTS keys.person  (
-		id VARCHAR(9) PRIMARY KEY,
-		name TEXT NOT NULL,
+DROP TABLE IF EXISTS keys.users;
+CREATE TABLE IF NOT EXISTS keys.users  (
+		username TEXT PRIMARY KEY,
 		password TEXT NOT NULL,
 		phone_number TEXT NOT NULL
 );
 
-DROP TABLE IF EXISTS keys."class";
-CREATE TABLE IF NOT EXISTS keys."class"  (
+DROP TABLE IF EXISTS keys."classes";
+CREATE TABLE IF NOT EXISTS keys."classes"  (
 	class_number VARCHAR(3) PRIMARY KEY,
 	has_projector Boolean NOT NULL DEFAULT FALSE,
 	has_computers Boolean NOT NULL DEFAULT FALSE
 );
 
-DROP TABLE IF EXISTS keys.schedule;
-CREATE TABLE IF NOT EXISTS keys.schedule  (
+DROP TABLE IF EXISTS keys.schedules;
+CREATE TABLE IF NOT EXISTS keys.schedules  (
 	id SERIAL PRIMARY KEY,
-	person_id VARCHAR(9) REFERENCES keys.person(id),
-	class_number VARCHAR(3) REFERENCES keys."class"(class_number),
+	username TEXT REFERENCES keys.users(username),
+	class_number VARCHAR(3) REFERENCES keys."classes"(class_number),
 	start_date TIMESTAMP NOT NULL,
 	end_date TIMESTAMP NOT NULL
 );
